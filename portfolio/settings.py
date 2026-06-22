49c346d6e356b4745b76b288fec79529272445bf
Ajay <ajaykrishnamsetty4@gmail.com>
2026-06-23 02:42:43 +0530

    fix static files for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
